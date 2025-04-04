5842b0dc67f3f1e74c9fd633a73471025c773252
Leonid <leo.chel@mail.ru>
2025-04-05 00:10:10 +0300

    add basic structure of a backend

---
+++ backend/src/test/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package vsu.tp5_3.techTrackInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrackInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
