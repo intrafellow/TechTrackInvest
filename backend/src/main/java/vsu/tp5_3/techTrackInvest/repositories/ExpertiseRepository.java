0dc95e70eb855147cc5e41d79f37faf7fbef0d73
Leonid <leo.chel@mail.ru>
2025-04-10 18:51:03 +0300

    TEC-34 add basic structure for the project

--- backend/src/main/java/vsu/tp5_3/techTrackInvest/repositories/ExpertiseRepository.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/repositories/ExpertiseRepository.java
@@ -1,7 +1,9 @@
-package vsu.tp5_3.techTrackInvest.repository;
+package vsu.tp5_3.techTrackInvest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vsu.tp5_3.techTrackInvest.entities.Expertise;
 
+@Repository
 public interface ExpertiseRepository extends JpaRepository<Expertise, Long> {
 }
--- backend/src/main/java/vsu/tp5_3/techTrackInvest/repositories/UserRepository.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/repositories/UserRepository.java
@@ -1,7 +1,9 @@
-package vsu.tp5_3.techTrackInvest.repository;
+package vsu.tp5_3.techTrackInvest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vsu.tp5_3.techTrackInvest.entities.AppUser;
 
+@Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 }
