42083b48191aad7ba5b7022c845b7793be53c484
Leonid <leo.chel@mail.ru>
2025-04-11 12:54:45 +0300

    update tests

--- backend/src/test/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplicationTests.java
+++ backend/src/test/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplicationTests.java
@@ -3,7 +3,9 @@ package vsu.tp5_3.techTrackInvest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class TechTrackInvestApplicationTests {
 
 	@Test
