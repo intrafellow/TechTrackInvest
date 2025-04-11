cba24360242ab155986ea0a78fd19ff788be72c6
Leonid <leo.chel@mail.ru>
2025-04-11 13:53:10 +0300

    test gh actions

--- backend/src/test/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplicationTests.java
+++ backend/src/test/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplicationTests.java
@@ -1,5 +1,6 @@
 package vsu.tp5_3.techTrackInvest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class TechTrackInvestApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+		Assertions.assertTrue(true);
 	}
 
 }
