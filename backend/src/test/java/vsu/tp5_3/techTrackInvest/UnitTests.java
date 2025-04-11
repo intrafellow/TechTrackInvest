cecbca7fe6491148d7eb1ec1f5e506712d716b4e
Leonid <leo.chel@mail.ru>
2025-04-11 14:32:38 +0300

    add unit test for check actions

--- backend/src/test/java/vsu/tp5_3/techTrackInvest/UnitTests.java
+++ backend/src/test/java/vsu/tp5_3/techTrackInvest/UnitTests.java
@@ -1,15 +1,11 @@
 package vsu.tp5_3.techTrackInvest;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TechTrackInvestApplicationTests {
+class UnitTests {
 
 	@Test
-	@Tag("test for test")
 	void testingMethod() {
 		Assertions.assertTrue(true);
 	}
