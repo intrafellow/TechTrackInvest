98281deb783e68d6e9524bda0b66d7b4d71eeb2f
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-04-13 18:00:55 +0300

    Empty dev branch

---
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/TechTrackInvestApplication.java
@@ -0,0 +1,18 @@
+package vsu.tp5_3.techTrackInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class TechTrackInvestApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(TechTrackInvestApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/entities/CrisisHistory.java
@@ -0,0 +1,21 @@
+package vsu.tp5_3.techTrackInvest.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "crisisHistory")
+@Setter
+@Getter
+@NoArgsConstructor
+public class CrisisHistory {
+    @Id
+    @Column(name = "crysis_resource_id")
+    private String crisisHistoryId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "session_id", nullable = false)
+    private Session session;
+}
