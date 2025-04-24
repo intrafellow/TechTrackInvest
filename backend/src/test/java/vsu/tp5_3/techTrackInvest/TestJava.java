d9b695504bc86273bbb3286b87e82fc4b890e720
Leonid <leo.chel@mail.ru>
2025-04-24 23:05:06 +0300

    change for testing actions

---
+++ backend/src/test/java/vsu/tp5_3/techTrackInvest/TestJava.java
@@ -0,0 +1,13 @@
+package vsu.tp5_3.techTrackInvest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestJava {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
