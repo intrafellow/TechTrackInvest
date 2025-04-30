f24a6f211b97a06e5d511ac6387b77e5f3f03d1a
intrafellow <145547700+intrafellow@users.noreply.github.com>
2025-04-30 12:32:41 +0300

    test: add file with syntax error to test pipeline

---
+++ front/src/test-error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestError: React.FC = () => {
+  return (
+    <div>
+      <h1>Тестовая страница с ошибкой</h1>
+      <p>Этот компонент содержит синтаксическую ошибку
+    </div>
+  );
+};
+
+export default TestError; 
