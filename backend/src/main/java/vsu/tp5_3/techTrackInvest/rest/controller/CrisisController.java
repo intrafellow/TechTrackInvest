77ec2b459401c13e7b08ca3bcb43b19b5e5f8b8e
Leonid <leo.chel@mail.ru>
2025-05-02 22:23:56 +0300

    TEC-76 add description about crisis controller

--- backend/src/main/java/vsu/tp5_3/techTrackInvest/rest/controller/CrisisController.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/rest/controller/CrisisController.java
@@ -1,5 +1,7 @@
 package vsu.tp5_3.techTrackInvest.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +11,17 @@ import org.springframework.web.server.ResponseStatusException;
 import vsu.tp5_3.techTrackInvest.dto.CrisisReadDto;
 import vsu.tp5_3.techTrackInvest.service.interfaces.CrisisService;
 
+@Tag(name = "Управление кризисами", description = "Основные методы получения и решение кризисов")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/crisis")
 public class CrisisController {
     private final CrisisService crisisService;
+
+    @Operation(
+            summary = "Получение случайного нового кризиса",
+            description = "В случайный момент времени клиент запрашивает кризис, чтобы предоставить игроку решение"
+    )
     @GetMapping()
     public ResponseEntity<CrisisReadDto> findById() {
         return crisisService.getCrisis()
@@ -23,6 +31,11 @@ public class CrisisController {
                         "Кризис не найден"
                 ));
     }
+
+    @Operation(
+            summary = "Выбираем решение кризиса",
+            description = "Передаем id варианта решения кризиса, которое мы выбрали, чтобы применить эффекты"
+    )
     @PostMapping("/solution/{solutionId}")
     public ResponseEntity<?> solve(@PathVariable String solutionId) {
         crisisService.solve(solutionId);
