cda1a29aef01e09b237a09b362b7b059e8666543
KostySCH <knsch20041208@gmail.com>
2025-05-05 14:41:27 +0300

    Add monthCount and stepCount API

--- backend/src/main/java/vsu/tp5_3/techTrackInvest/service/interfaces/MonthService.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/service/interfaces/MonthService.java
@@ -1,9 +1,17 @@
 package vsu.tp5_3.techTrackInvest.service.interfaces;
 
+import org.springframework.http.ResponseEntity;
+import vsu.tp5_3.techTrackInvest.dto.MonthCountDto;
 import vsu.tp5_3.techTrackInvest.dto.MonthEndDto;
+import vsu.tp5_3.techTrackInvest.dto.StepCountDto;
 
 import java.util.Optional;
 
 public interface MonthService {
     Optional<MonthEndDto> endMonth();
+
+    StepCountDto getStepCount();
+
+    MonthCountDto getMonthCount();
+
 }
