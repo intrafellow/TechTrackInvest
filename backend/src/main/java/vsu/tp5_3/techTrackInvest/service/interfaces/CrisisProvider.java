0170b4b99a22dea810c44d4efd17e9f11cc07e07
Leonid <leo.chel@mail.ru>
2025-05-22 01:00:18 +0300

    TEC-105 remove redundant public modifier in CrisisProvider interface

--- backend/src/main/java/vsu/tp5_3/techTrackInvest/service/interfaces/CrisisProvider.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/service/interfaces/CrisisProvider.java
@@ -3,5 +3,5 @@ package vsu.tp5_3.techTrackInvest.service.interfaces;
 import vsu.tp5_3.techTrackInvest.entities.mongo.CrisisMongo;
 
 public interface CrisisProvider {
-    public CrisisMongo getRandomCrisis();
+    CrisisMongo getRandomCrisis();
 }
