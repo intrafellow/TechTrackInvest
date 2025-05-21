46a6620aeb3cc50195338f4993ae2a07eea5e340
Leonid <leo.chel@mail.ru>
2025-05-22 00:59:01 +0300

    TEC-105 implement strategy class to get crisis fromm mongo

--- backend/src/main/java/vsu/tp5_3/techTrackInvest/service/implementations/strategy/CrisisMongoProvider.java
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/service/implementations/strategy/CrisisMongoProvider.java
@@ -1,6 +1,17 @@
 package vsu.tp5_3.techTrackInvest.service.implementations.strategy;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import vsu.tp5_3.techTrackInvest.entities.mongo.CrisisMongo;
+import vsu.tp5_3.techTrackInvest.repositories.mongo.CrisisMongoRepository;
 import vsu.tp5_3.techTrackInvest.service.interfaces.CrisisProvider;
 
+@Component
+@RequiredArgsConstructor
 public class CrisisMongoProvider implements CrisisProvider {
+    public final CrisisMongoRepository crisisMongoRepository;
+    @Override
+    public CrisisMongo getRandomCrisis() {
+        return crisisMongoRepository.findRandomEntity();
+    }
 }
