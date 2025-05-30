4fb9f37cdce6490d880bf88f620717a5defe98e2
Leonid <leo.chel@mail.ru>
2025-05-30 16:55:54 +0300

    TEC-114 add exception handling when user tries to load session when he doesnt have one

---
+++ backend/src/main/java/vsu/tp5_3/techTrackInvest/exceptions/SessionNotFoundException.java
@@ -0,0 +1,7 @@
+package vsu.tp5_3.techTrackInvest.exceptions;
+
+public class SessionNotFoundException extends RuntimeException {
+    public SessionNotFoundException(String message) {
+        super(message);
+    }
+}
